1e6e25385fae02d1a0519b4202f6743dfa55f7a2
[Marina] <[marisha071997@gmail.com]>
2023-04-03 16:42:31 +0300

    Логирование

--- utils/http_methods.py
+++ utils/http_methods.py
@@ -1,4 +1,5 @@
 import requests
+from utils.logger import Logger
 
 """Список http методов"""
 class Http_mothods:
@@ -7,21 +8,29 @@ class Http_mothods:
 
     @staticmethod
     def get(url):
+        Logger.add_request(url, method="GET")
         result = requests.get(url, headers=Http_mothods.headers, cookies=Http_mothods.cookie)
+        Logger.add_response(result)
         return result
 
     @staticmethod
     def post(url, body):
+        Logger.add_request(url, method="POST")
         result = requests.post(url, json=body, headers=Http_mothods.headers, cookies=Http_mothods.cookie)
+        Logger.add_response(result)
         return result
 
     @staticmethod
     def put(url, body):
+        Logger.add_request(url, method="PUT")
         result = requests.put(url, json=body, headers=Http_mothods.headers, cookies=Http_mothods.cookie)
+        Logger.add_response(result)
         return result
 
     @staticmethod
     def delete(url, body):
+        Logger.add_request(url, method="DELETE")
         result = requests.delete(url, json=body, headers=Http_mothods.headers, cookies=Http_mothods.cookie)
+        Logger.add_response(result)
         return result
     
